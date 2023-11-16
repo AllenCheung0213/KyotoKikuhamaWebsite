f52b99af6825bfa3b573acc355a4a9bf2e8ba9dc
rubak <ruba.khan2014@gmail.com>
2023-11-16 21:03:25 +0900

    finished top section + half of kikuhama section

--- kikuhama_website/src/Content.js
+++ kikuhama_website/src/Content.js
@@ -27,9 +27,14 @@ const Content = () => {
                     <div className = "text-description">
                         <p>
                             Welcome to the Kikuhama district of Kyoto! Our project is focused on mapping and assessing the current status of this historic neighborhood. We aim to develop thoughtful revitalization proposals that prioritize sustainability, preserve cultural heritage, and enhance community values. The goal is to create a more vibrant, attractive Kikuhama for all - residents, workers, businesses, and visitors alike.
-                            Together, we can foster a thriving community here in Kikuhama that appeals to the needs and preferences of all who live, work and visit this charming district. With cooperation and fresh thinking, we can make Kikuhama an even better place to call home, set up shop, or spend time exploring. Join us as we map the future of Kikuhama!
+                            Together, we can foster a thriving community here in Kikuhama that appeals to the needs and preferences of all who live, work and visit this charming district. With cooperation and fresh thinking, we can make Kikuhama an even better place to call home, set up shop, or spend time exploring.
                         </p>
                     </div>
+                </div>
+                <h3>Projects Over the Years (2019 - 2023)</h3>
+                <div className = "over-the-years">
+                    <embed src={yourPDFDocument} type="application/pdf" width="100%" height="600px" />
+
                 </div>
             </div>
             <div id="projects">
